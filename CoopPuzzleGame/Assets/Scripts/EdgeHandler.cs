c1c16528504b4d2cd6373cab4fa32cd6d0a0c0ab
sophie.amelin <42768125+sophie333@users.noreply.github.com>
2018-10-08 10:59:49 +0100

    Character Movement
    created CharacterController Script

---
+++ CoopPuzzleGame/Assets/Scripts/EdgeHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeHandler : MonoBehaviour {
+
+    // Use this for initialization
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController player = other.GetComponent<PlayerController>();
+
+        if(player)
+        {
+            //not y rotation 
+
+        }
+    }
+}
---
+++ CoopPuzzleGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float constantSpeed;
+
+    private Rigidbody rbody;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 gravity = new Vector3(0, -120f, 0);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rbody.AddForce(movement * constantSpeed + gravity);
+    }
+}
