6a25ebca3aed9784292e55545f3cdd9275d2a257
sophie.amelin <42768125+sophie333@users.noreply.github.com>
2018-10-15 01:39:45 +0200

    Tried: Walking across edges

--- CoopPuzzleGame/Assets/Scripts/EdgeHandler.cs
+++ CoopPuzzleGame/Assets/Scripts/EdgeHandler.cs
@@ -11,8 +11,11 @@ public class EdgeHandler : MonoBehaviour {
 
         if(player)
         {
-            //not y rotation 
-
+            //player.SetGravity(0);
+            //not y rotation
+           // Vector3 eulerRotation = new Vector3(transform.eulerAngles.x, player.transform.eulerAngles.y, player.transform.eulerAngles.z);
+           // player.transform.rotation = Quaternion.Euler(eulerRotation);
+            //player.SetGravity(-120f);
         }
     }
 }
--- CoopPuzzleGame/Assets/Scripts/PlayerController.cs
+++ CoopPuzzleGame/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     public float constantSpeed;
 
+    //private Vector3 gravity = new Vector3(0, -120f, 0);
+    private float gravity = -120f;
+    private float lerpSpeed = 10; // smoothing speed
+    private Vector3 surfaceNormal; //current surface normal
+
+    //player variables
+    private Vector3 playerNormal; // player normal
     private Rigidbody rbody;
+    private Transform m_transform;
 
     void Start()
     {
         rbody = GetComponent<Rigidbody>();
+        m_transform = transform;
+        playerNormal = transform.up;
     }
 
     void FixedUpdate()
@@ -18,9 +28,46 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 gravity = new Vector3(0, -120f, 0);
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rbody.AddForce(movement * constantSpeed + gravity);
+        rbody.AddForce(movement * constantSpeed); //+ gravity
+        //apply constant weight force according to player normal
+        rbody.AddForce(gravity * rbody.mass * playerNormal);
+    }
+
+    private void Update()
+    {
+        //update surface normal
+        Ray ray = new Ray(m_transform.position, -playerNormal);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        { //use it to update surface normal
+            Debug.Log("yej");
+            surfaceNormal = hit.normal;
+        }
+        else
+        {
+            Debug.Log("nop");
+            surfaceNormal = Vector3.up;
+        }
+        playerNormal = Vector3.Lerp(playerNormal, surfaceNormal, lerpSpeed * Time.deltaTime);
+    }
+    /* public void SetGravity(float y)
+     {
+         gravity = new Vector3(0, y, 0);
+     }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Ray ray = new Ray(m_transform.position, m_transform.for)
+    }
+
+        public void CrossEdge()
+    {
+        rbody.isKinematic = true;
+        Vector3 originalPos = m_transform.position;
+        Quaternion originalRot = m_transform.rotation;
+
     }
 }
