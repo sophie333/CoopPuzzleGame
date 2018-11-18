59800c62c356892a8951991451789e4d8e225c7d
sophie.amelin <42768125+sophie333@users.noreply.github.com>
2018-11-19 00:09:52 +0100

    Added platform behavior scripts & animation
    script 1 : platform disappears
    script 2 : platform moves to another direction using animation

---
+++ CoopPuzzleGame/Assets/Scripts/MovPlatform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovPlatform : MonoBehaviour
+{
+    [SerializeField] private GameObject obj;
+    [SerializeField] private Animator move;
+    //[SerializeField] private Animation moveBack;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        move.Play("moveTo");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        move.Play("moveBack");
+    }
+}
