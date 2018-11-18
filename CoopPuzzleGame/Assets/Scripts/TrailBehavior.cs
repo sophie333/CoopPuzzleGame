a40306c2a13b4c35cce54b7a2b0ecff8bd4ec741
sophie.amelin <42768125+sophie333@users.noreply.github.com>
2018-11-18 19:32:03 +0100

    Fixed Trail Movement
    new script for movement

---
+++ CoopPuzzleGame/Assets/Scripts/TrailBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailBehavior : MonoBehaviour {
+
+    [SerializeField]
+    private Transform playerTrans;
+
+    private Transform m_transform;
+
+    private void Start()
+    {
+        m_transform = transform;
+    }
+    
+    void Update ()
+    {
+        if (this.tag == "player1")
+        {
+            m_transform.position = new Vector3(playerTrans.position.x + 30.0f, playerTrans.position.y - 0.9f, playerTrans.position.z);
+        }
+        if (this.tag == "player2")
+        {
+            m_transform.position = new Vector3(playerTrans.position.x - 30.0f, playerTrans.position.y - 0.9f, playerTrans.position.z);
+        }
+	}
+}
