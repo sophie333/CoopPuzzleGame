eae9f5cb6904e47355286b1aa0db21d23277f494
sophie.amelin <42768125+sophie333@users.noreply.github.com>
2018-11-22 17:10:12 +0100

    Added trigger behavior
    laser disable behavior

---
+++ CoopPuzzleGame/Assets/Scripts/DisObjectTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisObjectTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject obj;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        obj.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        obj.SetActive(true);
+    }
+}
