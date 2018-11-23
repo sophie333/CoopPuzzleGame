d595bcda9a736fc9316478e4f9b8bed62e1d56ef
sophie.amelin <42768125+sophie333@users.noreply.github.com>
2018-11-23 01:27:26 +0100

    added trigger visuals and skybox
    trigger reacts when being triggered, skybox is milkyway

--- CoopPuzzleGame/Assets/Scripts/MovPlatformTrigger.cs
+++ CoopPuzzleGame/Assets/Scripts/MovPlatformTrigger.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class MovPlatformTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject obj;
+    [SerializeField] private GameObject pSystem;
     [SerializeField] private Animator move;
     //[SerializeField] private Animation moveBack;
+    [SerializeField] private Light pLight;
+
+    private Material m_Material;
+    private Color m_Color;
+
+    private void Start()
+    {
+        m_Material = GetComponent<Renderer>().material;
+        m_Color = m_Material.color;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         move.Play("moveTo");
+        pSystem.SetActive(false);
+        pLight.intensity = 0;
+        m_Material.color = Color.gray;
     }
 
     private void OnTriggerExit(Collider other)
     {
         //move.Play("moveBack");
+        pSystem.SetActive(true);
+        pLight.intensity = 6.8f;
+        m_Material.color = m_Color;
     }
 
     /*private IEnumerator ChangeLayer()
