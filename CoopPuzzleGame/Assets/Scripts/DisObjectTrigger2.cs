74a099220700c5ebc654f1d12d3705b9aada8bd6
sophie.amelin <42768125+sophie333@users.noreply.github.com>
2018-11-23 16:48:23 +0000

    changed box behavior

---
+++ CoopPuzzleGame/Assets/Scripts/DisObjectTrigger2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisObjectTrigger2 : MonoBehaviour
+{
+    [SerializeField] private GameObject obj;
+    [SerializeField] private GameObject pSystem;
+    [SerializeField] private Light pLight;
+    [SerializeField] private AudioSource audioUp;
+    [SerializeField] private AudioSource audioDown;
+
+    private Material m_Material;
+    private Color m_Color;
+
+    private void Start()
+    {
+        m_Material = GetComponent<Renderer>().material;
+        m_Color = m_Material.color;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        obj.SetActive(false);
+        pSystem.SetActive(false);
+        pLight.intensity = 0;
+        m_Material.color = Color.gray;
+        audioUp.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        pSystem.SetActive(true);
+        pLight.intensity = 6.8f;
+        m_Material.color = m_Color;
+        audioDown.Play();
+    }
+}
