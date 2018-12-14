c9a15d46f2d1c3da551d267ec8f19009b000f9d5
Sophie Amelin <42768125+sophie333@users.noreply.github.com>
2018-12-14 15:43:27 +0000

    added smoother camera movement

--- CoopPuzzleGame/Assets/Scripts/CameraMovement.cs
+++ CoopPuzzleGame/Assets/Scripts/CameraMovement.cs
@@ -29,34 +29,61 @@ public class CameraMovement : MonoBehaviour
         m_transform.position = m_player.transform.position + offsetPosition;
     }*/
 
-    [SerializeField] private PlayerController1 m_player;
+    [SerializeField] private Transform m_playerTrans;
+    [SerializeField] private Transform m_cam;
     private Transform m_transform;
     private float m_angleX;
+    private float m_angleY;
     private float m_distance;
     private float height;
-    private Transform m_playerTrans;
+    public float xRot = 30;
+    public float yRot = 0;
+    public float zRot = 0;
 
     private void Start()
     {
         m_transform = transform;
         m_angleX = m_transform.rotation.eulerAngles.x;
-        m_playerTrans = m_player.transform;
-        m_distance = -39.0f;
+        m_angleY = m_transform.rotation.eulerAngles.y;
+        m_distance = 100;
         height = 63.0f;
     }
+    
+    //void LateUpdate()
+    //{
+    //    //we get the rotation angle of the player
+    //    float angleY = m_playerTrans.rotation.eulerAngles.y;
+    //    float angleX = m_playerTrans.rotation.eulerAngles.x;
+
+    //    //we rotate the camera with the same angle
+    //    m_transform.rotation = Quaternion.Euler(new Vector3(m_angleX, angleY, 0.0f));
+    //   // m_transform.rotation *= Quaternion.Euler(new Vector3(angleX, m_angleY, 0.0f));
+
+    //    //we calculate the cartesian coordinates using the polar coordinates
+    //    Vector3 offsetPosition = new Vector3(-m_distance * Mathf.Sin(angleY * Mathf.Deg2Rad), height, -m_distance * Mathf.Cos(angleY * Mathf.Deg2Rad));
+
+    //    //we use the player position plus the offset calculated above
+    //    m_transform.position = m_playerTrans.position + offsetPosition;
+    //}
 
     void LateUpdate()
     {
-        //we get the rotation angle of the player
-        float angle = m_playerTrans.rotation.eulerAngles.y;
+     
+        m_transform.position = m_playerTrans.position;
+        
+        //Vector3 wantedPosition = m_transform.localPosition + (m_playerTrans.up * height + m_playerTrans.forward * -m_distance);
+        //m_transform.localPosition = Vector3.Slerp(m_transform.localPosition, wantedPosition, 0.9f);
 
-        //we rotate the camera with the same angle
-        m_transform.rotation = Quaternion.Euler(new Vector3(m_angleX, angle, 0.0f));
+        m_transform.localPosition += m_playerTrans.up * height + m_playerTrans.forward * -m_distance;
 
-        //we calculate the cartesian coordinates using the polar coordinates
-        Vector3 offsetPosition = new Vector3(-m_distance * Mathf.Sin(angle * Mathf.Deg2Rad), height, -m_distance * Mathf.Cos(angle * Mathf.Deg2Rad));
+        m_transform.rotation = Quaternion.Slerp(m_transform.rotation, m_playerTrans.rotation, 0.7f);
+        //m_cam.LookAt(m_playerTrans);
+        //m_cam.eulerAngles += new Vector3(30.0f, 0.0f, 0.0f);
+        //m_transform.LookAt(m_playerTrans);
+        //m_transform.eulerAngles += new Vector3(xRot, yRot, zRot);
+        //Vector3 rotVect = m_transform.localRotation.eulerAngles;
 
-        //we use the player position plus the offset calculated above
-        m_transform.position = m_playerTrans.position + offsetPosition;
+        //Quaternion rot = Quaternion.Euler(rotVect.x, rotVect.y, rotVect.z);
+        //m_transform.localRotation = rot;
     }
 }
